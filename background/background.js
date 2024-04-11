7599943145287d757f4284f941d27071d055ba53
vzneyv <132143070+vzneyv@users.noreply.github.com>
2024-04-11 18:41:41 +0100

    Update background.js

--- background/background.js
+++ background/background.js
@@ -1,23 +1,30 @@
+// Create context menu item
 chrome.contextMenus.create({
-    id: "convertLink",
-    title: "Convert link with PMT Link Converter",
-    contexts: ["link"]
-  });
-  
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "convertLink") {
-      convert(info.linkUrl);
-    }
-  });
-  
-  function convert(url) {
-    let convertedUrl = "";
-    if (url.startsWith("https://www.physicsandmathstutor.com/") && url.includes("pdf-pages/?pdf=")) {
-      convertedUrl = decodeURIComponent(url.substring(url.indexOf("pdf-pages/?pdf=") + 15));
-    } else {
-      alert("Invalid PMT link. Please enter a correct URL.");
-      return;
-    }
-    chrome.tabs.create({ url: convertedUrl });
+  id: "convertLink",
+  title: "Convert link with PMT Link Converter",
+  contexts: ["link"]
+});
+
+// Add listener for context menu item click
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "convertLink") {
+    convert(info.linkUrl);
   }
-  
+});
+
+// Add listener for web navigation to detect valid PMT links
+chrome.webNavigation.onCompleted.addListener(function(details) {
+  if(details.url.startsWith("https://www.physicsandmathstutor.com/") && details.url.includes("pdf-pages/?pdf=")) {
+    convert(details.url);
+  }
+});
+
+// Function to convert PMT link
+function convert(url) {
+  if (url.startsWith("https://www.physicsandmathstutor.com/") && url.includes("pdf-pages/?pdf=")) {
+    let convertedUrl = decodeURIComponent(url.substring(url.indexOf("pdf-pages/?pdf=") + 15));
+    chrome.tabs.update({ url: convertedUrl });
+  } else {
+    alert("Invalid PMT link. Please enter a correct URL.");
+  }
+}
